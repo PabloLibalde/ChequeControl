cf7e7c5ac4eb6fa05515924c64053630cd31fde5
PabloLibalde <pablolibalde@gmail.com>
2023-01-26 22:37:58 -0300

    Teste

---
+++ cadastros.py
@@ -0,0 +1,27 @@
+# Class inicial do cheque e suas propriedades
+class Pessoas:
+    def __init__(self, pessoa_cpf,pessoa_nome,pessoa_apelido,pessoa_endereco,pessoa_bairro):
+        self.pessoa_cpf = pessoa_cpf
+        self.pessoa_nome = pessoa_nome
+        self.pessoa_apelido = pessoa_apelido
+        self.pessoa_endereco = pessoa_endereco
+        self.pessoa_bairro = pessoa_bairro
+
+class Bancos:
+    def __init__(self,banco_nome):
+        self.banco_nome = banco_nome        
+
+class Contas:
+    def __init__(self, conta_banco,conta_numero,conta_agencia):
+        self.conta_banco = conta_banco
+        self.conta_numero = conta_numero
+        self.conta_agencia = conta_agencia
+
+class Cheques:
+    def __init__(self, cheque_numero,cheque_banco,cheque_agencia,cheque_conta,cheque_nome):
+        self.cheque_numero = cheque_numero
+        self.cheque_banco = cheque_banco
+        self.cheque_agencia = cheque_agencia
+        self.cheque_conta = cheque_conta
+        self.cheque_nome = cheque_nome
+
