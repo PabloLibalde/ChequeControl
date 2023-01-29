941fb0909512a4bd7cb1663620f2aa1d7f37ec83
PabloLibalde <pablolibalde@gmail.com>
2023-01-29 20:34:44 -0300

    teste

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from cadastros import (Pessoas,Cadastrar,Deletar)
 from conectionbd import (DataBase)
 
-p = Pessoas('null','107.978.217.67','Pablo Libalde','Pablo','Rua Argeu Resende, 96','Centro')   
+
+nome = input("Digite Seu nome Completo:")
+apelido = input("Agora seu Apelido:")
+cpf = input("Seu CPF:")
+endereco = input("Seu Endereço:")
+bairro = input("Por ultimo /n Seu Bairro:")
+
+
+
+p = Pessoas('null',f'{cpf}',f'{nome}',f'{apelido}',f'{endereco}',f'{bairro}')   
 
 db = DataBase()
 db.connect()
@@ -9,25 +18,44 @@ db.create_table_pessoas()
 db.close_connection()
 
 
-# pessoa_input = (p.cpf,p.nome,p.apelido,p.endereco,p.bairro)
+pessoa_input = (p.cpf,p.nome,p.apelido,p.endereco,p.bairro)
 # #pessoa_input = (p.nome)
 
 
 # j = (1, '107.978.217.67', 'Pablo Libalde', 'Pablo', 'Rua Argeu Resende, 96', 'Centro')
 
 
-# Cadastrar.pessoa(pessoa_input)
+Cadastrar.pessoa(pessoa_input)
 
-# # print(db.select_all_pessoas())
+print(db.select_all_pessoas())
 
-x = db.select_all_pessoas()
-for y in x:
-    # print(y)
-    lista =['cpf','nome','apelido','endereco','bairro','id']
-    # print(lista[0])
-    dic={}
-    for col in y:
-        print(col)
-    #     p= 0
-        dic[f'{lista[p]}']=col
-    # print(dic)
+# x = db.select_all_pessoas()
+# for y in x:
+#     # print(y)
+#     lista =['cpf','nome','apelido','endereco','bairro','id']
+#     # print(lista[0])
+#     dic={}
+#     for col in y:
+#         print(col)
+#     #     p= 0
+#         dic[f'{lista[p]}']=col
+#     # print(dic)
+
+# Pablo = Pessoas('107.978.217.67','Pablo Libalde','Pablo','Rua Argeu Resende','Centro')
+# Ketully = Pessoas('98989898','Ketully Brunow','Ketully','Predio da Zinsk','Populares')
+# # print(Pablo.nome,Pablo.cpf)
+
+# def nome(nome):
+#     print('-'*20)
+#     print(f'Pessoa       {nome}')    
+#     print('-'*20)
+    
+    
+
+nome(f'{Ketully.nome}')
+nome(f'{Pablo.nome}')
+nome('Feio_Raquelle')
+nome('Feio_Cassio')
+nome('Feio_Loren')
+
+{''}
